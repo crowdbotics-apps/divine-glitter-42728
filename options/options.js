3a036005a9102a6704af7cb58e40ea68ec72b7ee
Crowdbotics <team@crowdbotics.com>
2023-08-08 15:25:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-glitter-42728",
+  url: "https://divine-glitter-42728.botics.co",
+  api: "https://divine-glitter-42728.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
